9c6144662af6f8e8a0dedadb4b95aadac43c10cc
“Elie <elie.maj@hotmail.com>
2022-06-04 01:09:41 +0300

    fix(pofile)fix db name on connetion

--- Backend/connection.php
+++ Backend/connection.php
@@ -3,7 +3,7 @@ header('Access-Control-Allow-Origin: *');
 $host = "localhost";
 $db_user = "root";
 $db_pass = null;
-$db_name = "facebook";
+$db_name = "feedbook";
 
 $mysqli = new mysqli($host, $db_user, $db_pass, $db_name);
 
