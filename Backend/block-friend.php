5282e8fc036b26f75875743586e5fb93d2fbbcc0
“Elie <elie.maj@hotmail.com>
2022-06-04 15:14:34 +0300

    add(profile)add block function

---
+++ Backend/block-friend.php
@@ -0,0 +1,9 @@
+<?php
+include("connection.php");
+
+$user_id = $_GET["id"];
+$friend_id = $_GET["friend_id"];
+$query = $mysqli->prepare("UPDATE relationships SET relation= -1 WHERE friend_id = ? and user_id = ?");
+$query->bind_param("ii", $friend_id, $user_id);
+$query->execute();
+?>
