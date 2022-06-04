7e3c3aa237515752d5328dc7660b970b930aaf65
“Elie <elie.maj@hotmail.com>
2022-06-04 14:44:34 +0300

    add(profile)add show comments of posts for thr user

---
+++ Backend/show-comments-of-post.php
@@ -0,0 +1,14 @@
+<?php
+include("connection.php");
+$id=$_get["post_id"];
+
+$query = $mysqli->prepare("SELECT c.comment, u.full_name FROM comments c INNER join users u on u.id = c.user_id WHERE c.post_id = ? ORDER BY c.id DESC");
+$query->bind_param("i", $id);
+$query->execute();
+$result = $query->get_result();
+$jason= array();
+while($row = $result->fetch_assoc()){
+    $jason[] = $row;
+}
+echo json_encode($jason);
+?>
