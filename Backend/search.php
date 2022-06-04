9875554d44d309cba5bbe845f21417e109cbecfb
“Elie <elie.maj@hotmail.com>
2022-06-04 15:49:59 +0300

    fix(profile)fix search result excluding blocked only

--- Backend/search.php
+++ Backend/search.php
@@ -4,7 +4,7 @@ include("connection.php");
 $name = $_GET["name"];
 
 
-$query = $mysqli->prepare("SELECT * FROM users where (username LIKE concat('%',?,'%') OR full_name LIKE concat('%',?,'%') OR email LIKE ?)");
+$query = $mysqli->prepare("SELECT * FROM users u where (username LIKE concat('%',?,'%') OR full_name LIKE concat('%',?,'%') OR email LIKE ?)AND  u.id != (SELECT r.friend_id from users u INNER JOIN relationships r ON r.user_id = u.id WHERE r.relation =-1)");
 $query->bind_param("sss", $name,$name,$name);
 $query->execute();
 $array = $query->get_result();
